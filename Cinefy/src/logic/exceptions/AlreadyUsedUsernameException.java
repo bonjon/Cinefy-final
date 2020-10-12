b0e93f7e0eed8bf3aa0a6397f9d3e0fc71dfcf55
bonjon <45831678+bonjon@users.noreply.github.com>
2020-10-12 16:52:02 +0200

    Delete all

--- Cinefy/src/logic/exceptions/AlreadyUsedUsernameException.java
+++
@@ -1,9 +0,0 @@
-package logic.exceptions;
-
-public class AlreadyUsedUsernameException extends Exception{
-	private static final long serialVersionUID = 1L;
-	
-	public AlreadyUsedUsernameException(String message) {
-		super(message);
-	}
-}
