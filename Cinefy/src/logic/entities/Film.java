a4cc9b01a6dc4d66d0fae59a2f2459b4bb01ae44
bonjon <45831678+bonjon@users.noreply.github.com>
2021-01-04 23:37:17 +0100

    Film code smell

--- Cinefy/src/logic/entities/Film.java
+++ Cinefy/src/logic/entities/Film.java
@@ -14,21 +14,61 @@ public class Film {
 	private int anno;
 	private String nazione;
 	private String genere;
-
-	public Film(int filmId) {
-		this.filmId = filmId;
+	
+	// Utilizzo un pattern Builder poichè l'entity Film ha 8 parametri quindi un numero di parametri abbastanza elevato
+	
+	public static class FilmBuilder {
+		
+		private int filmId;
+		private String titolo;
+		private String url;
+		private String regista;
+		private String attore;
+		private int anno;
+		private String nazione;
+		private String genere;
+		
+		public FilmBuilder(int filmId, String titolo, String url, String regista) {
+			this.filmId = filmId;
+			this.titolo = titolo;
+			this.url = url;
+			this.regista = regista;
+		}
+		
+		public FilmBuilder setAttore(String attore) {
+			this.attore = attore;
+			return this;
+		}
+		
+		public FilmBuilder setAnno(int anno) {
+			this.anno = anno;
+			return this;
+		}
+		
+		public FilmBuilder setNazione(String nazione) {
+			this.nazione = nazione;
+			return this;
+		}
+		
+		public FilmBuilder setGenere(String genere) {
+			this.genere = genere;
+			return this;
+		}
+		
+		public Film build() {
+			return new Film(this);
+		}
 	}
-
-	public Film(int filmId, String titolo, String url, String regista, String attore, int anno, String nazione,
-			String genere) {
-		this.filmId = filmId;
-		this.titolo = titolo;
-		this.url = url;
-		this.regista = regista;
-		this.attore = attore;
-		this.anno = anno;
-		this.nazione = nazione;
-		this.genere = genere;
+	
+	private Film(FilmBuilder builder) {
+		this.filmId = builder.filmId;
+		this.titolo = builder.titolo;
+		this.url = builder.url;
+		this.regista = builder.regista;
+		this.attore = builder.attore;
+		this.anno = builder.anno;
+		this.nazione = builder.nazione;
+		this.genere = builder.genere;
 	}
 
 	public int getFilmId() {
